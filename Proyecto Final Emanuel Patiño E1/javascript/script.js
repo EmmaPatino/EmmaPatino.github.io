4d1e59de0ef55c627ca1d3aa9545589f36797da5
Emanuel <elemmap14@gmail.com>
2023-03-08 16:56:43 -0300

    miPrimerCommit

---
+++ Proyecto Final Emanuel Patiño E1/javascript/script.js
@@ -0,0 +1,40 @@
+document.getElementById("listaContacto-click1").onmouseover = function () {
+    document.getElementById("listaContacto-click1").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click1").innerHTML = "elemmap@hotmail.es";
+}
+document.getElementById("listaContacto-click1").onmouseout = function () {
+    document.getElementById("listaContacto-click1").style.color = "#0a144d";
+    document.getElementById("listaContacto-click1").innerHTML = "Email: elemmap@hotmail.es";
+}
+document.getElementById("listaContacto-click2").onmouseover = function () {
+    document.getElementById("listaContacto-click2").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click2").innerHTML = "3436-613853";
+}
+document.getElementById("listaContacto-click2").onmouseout = function () {
+    document.getElementById("listaContacto-click2").style.color = "#0a144d";
+    document.getElementById("listaContacto-click2").innerHTML = "Teléfono: 3436-613853";
+}
+document.getElementById("listaContacto-click3").onmouseover = function () {
+    document.getElementById("listaContacto-click3").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click3").innerHTML = "Victoria, Entre Rios - Argentina";
+}
+document.getElementById("listaContacto-click3").onmouseout = function () {
+    document.getElementById("listaContacto-click3").style.color = "#0a144d";
+    document.getElementById("listaContacto-click3").innerHTML = "Localidad: Victoria, Entre Rios - Argentina";
+}
+document.getElementById("listaContacto-click4").onmouseover = function () {
+    document.getElementById("listaContacto-click4").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click4").innerHTML = " Pte. Peron 481";
+}
+document.getElementById("listaContacto-click4").onmouseout = function () {
+    document.getElementById("listaContacto-click4").style.color = "#0a144d";
+    document.getElementById("listaContacto-click4").innerHTML = "Dirección: Pte. Peron 481";
+}
+document.getElementById("listaContacto-click5").onmouseover = function () {
+    document.getElementById("listaContacto-click5").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click5").innerHTML = "3153";
+}
+document.getElementById("listaContacto-click5").onmouseout = function () {
+    document.getElementById("listaContacto-click5").style.color = "#0a144d";
+    document.getElementById("listaContacto-click5").innerHTML = "Código Postal: 3153";
+}
